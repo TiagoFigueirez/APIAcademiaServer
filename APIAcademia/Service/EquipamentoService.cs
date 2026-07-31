a848a52715e79d66b7104fa5f602bf615b0c9600
Tiago Figueredo <tiago.figueiredo058@gmail.com>
2026-07-31 16:56:35 -0300

    feito alguns dos services

---
+++ APIAcademia/Service/EquipamentoService.cs
@@ -0,0 +1,63 @@
+﻿using APIAcademia.Model;
+using APIAcademia.Repositories.Interface;
+using APIAcademia.Service.Interface;
+
+namespace APIAcademia.Service
+{
+    public class EquipamentoService : IEquipamentoService
+    {
+        private readonly IUnitOfWork _wof;
+
+        public EquipamentoService(IUnitOfWork wof)
+        {
+            _wof = wof;
+        }
+
+        public async Task<IEnumerable<Equipamento>> GetAllEquipamento()
+        {
+            return await _wof.EquipamentosRepository.GetAllAsync();
+        }
+
+        public async Task<Equipamento> GetEquipamento(int id)
+        {
+            var equipamento = await _wof.EquipamentosRepository.GetAsync(e => e.Id == id);
+
+            if (equipamento == null)
+                throw new KeyNotFoundException("Equipamento não encontrado");
+
+            return equipamento;
+        }
+        public async Task<Equipamento> EquipamentoCreate(Equipamento equipamento)
+        {
+            var equipamentoCriado = _wof.EquipamentosRepository.Create(equipamento);
+            await _wof.Commit();
+
+            return equipamentoCriado;
+        }
+        public async Task<Equipamento> EquipamentoUpdate(int id, Equipamento equipamento)
+        {
+            var equipamentoUpdate = await _wof.EquipamentosRepository.GetAsync(e => e.Id==id);
+
+            if (equipamento == null)
+                throw new KeyNotFoundException("Equipamento não encontrado");
+
+            var equipamentoUpdated = _wof.EquipamentosRepository.Update(equipamento);
+            await _wof.Commit();
+
+            return equipamentoUpdated;
+        }
+
+        public async Task<Equipamento> EquipamentoDelete(int id)
+        {
+            var equipamentoDelete = await _wof.EquipamentosRepository.GetAsync(a => a.Id == id);
+
+            if (equipamentoDelete == null)
+                throw new KeyNotFoundException("Aluno não encontrado para deletar");
+
+            _wof.EquipamentosRepository.Delete(equipamentoDelete);
+            await _wof.Commit();
+
+            return equipamentoDelete;
+        }
+    }
+}
---
+++ APIAcademia/Service/ExercicioService.cs
@@ -0,0 +1,58 @@
+﻿using APIAcademia.Model;
+using APIAcademia.Repositories.Interface;
+using APIAcademia.Service.Interface;
+
+namespace APIAcademia.Service
+{
+    public class ExercicioService : IExercicioService
+    {
+        private readonly IUnitOfWork _wof;
+
+        public ExercicioService(IUnitOfWork wof)
+        {
+            _wof = wof;
+        }
+
+        public async Task<IEnumerable<Exercicio>> GetAllExercicio()
+        {
+            return await _wof.ExerciciosRepository.GetAllAsync();
+        }
+
+        public async Task<Exercicio> GetExercicio(int id)
+        {
+            var exercicio = await _wof.ExerciciosRepository.GetAsync(e => e.Id == id);
+
+            if (exercicio == null)
+                throw new KeyNotFoundException("Exercicio não encontrado");
+
+            return exercicio;
+        }
+
+        public async Task<Exercicio> ExercicioCreate(Exercicio exercicio)
+        {
+            var exercicioCriado = _wof.ExerciciosRepository.Create(exercicio);
+            await _wof.Commit();
+
+            return exercicioCriado;
+        }
+
+        public async Task<Exercicio> ExercicioUpdate(int id, Exercicio exercicio)
+        {
+            var exercicioUpdate = await _wof.EquipamentosRepository.GetAsync(e => e.Id == id);
+
+            if (exercicio == null)
+                throw new KeyNotFoundException("Equipamento não encontrado");
+
+            var exercicioUpdated = _wof.ExerciciosRepository.Update(exercicio);
+            await _wof.Commit();
+
+            return exercicioUpdated;
+        }
+
+        public Task<Exercicio> ExercicioDelete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
